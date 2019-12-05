8b5d080150809008fdfcf59173b4b353fd3657d3
tarunshrm864 <tarunshrm864@gmail.com>
2019-12-05 10:17:34 +0530

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './Header.js';
 
-function App() {
-  return (
-    <div>
-      <div className="header">
-        Phone Directory
-      </div>
-      <button>Add</button>
+class App extends Component {
+  render() {
+    return (
       <div>
-        <span>Name</span><br/>
-        <span>Phone</span>
+        <Header/>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
